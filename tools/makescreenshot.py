1d8efd234be028b7ff8678e3abb293c6eb2d5e9e
Dag0b3rtDuck <zdxii1vs1@gmail.com>
2019-02-15 22:21:17 +0100

    Addet screenshot tool

--- tools/makescreenshot.py
+++ tools/makescreenshot.py
@@ -3,10 +3,10 @@ import requests
 
 def take_scren():
     image = im = pyscreenshot.grab()
-    im.save("C:\\Users\\timoz\\Pictures\\screenshot.jpg")
+    im.save("C:\\Users\\username\\Pictures\\screenshot.jpg") #insert your username instead of username
     im.show()
     file = {
-        "file": open("C:\\Users\\timoz\\Pictures\\screenshot.jpg", "rb")
+        "file": open("C:\\Users\\username\\Pictures\\screenshot.jpg", "rb") #insert your username instead of username
     }
     key = {'api_key': 'hjsdg79qqerwg34uighölvng79'}
     requests.post("http://127.0.0.1:1337/api/upload", data=key, files=file)
